59b88d0f4cbaa6682ca87f294d84a7726346fcdb
Jordi Fernandez <jordi@ywarp.com>
2017-10-31 14:33:00 +0000

    Change logic to have access to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,7 @@ class PagesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: %i[index support coming_soon form]
 
-  def index
-    if user_signed_in?
-      redirect_to controller: :dashboard, action: :tutorials
-    end    
+  def index    
   end
   
   def support

0b1da77108cd55f6a4f972fb58d2223986e1b5b7
Jordi Fernandez <jordi@ywarp.com>
2017-11-02 18:48:20 +0000

    Add specs for static pages

--- spec/features/pages/static_pages_spec.rb
+++ spec/features/pages/static_pages_spec.rb
@@ -1,34 +1,60 @@
-# continue here
-describe "Static pages" do
-
-  describe "Home page" do
-    
-    before { 
-      visit root_path
-    }
-    
-    it 'finds front-page selector' do
-      expect(page).to have_selector :css, '#front-page'
-    end
-    
-    it 'returns status code 200' do
-      page_ok
-    end
-  end
+describe "Static pages", js: true do
   
-  describe "Support" do
-    
-    before { 
-      visit root_path
-    }
-    
-    it 'finds front-page selector' do
-      expect(page).to have_selector :css, '.front-page'
+  context "when user is not logged in" do
+
+    describe "Home page" do
+      
+      it 'finds selector' do
+        visit root_path
+        expect(page).to have_selector :css, '#front-page'
+      end
+      
+      it 'returns status code 200' do
+        visit root_path
+        page_ok
+      end
     end
     
-    it 'returns status code 200' do
-      page_ok
+    describe "Support" do
+      
+      it 'finds selector in page' do
+        visit root_path
+        find(".support").click
+        expect(page).to have_selector :css, '#support'
+      end
+    
     end
-  end
+  
+  end 
+  
+  context "when user is logged in" do
+    
+    describe "Support" do
+      
+      sign_as
+    
+      it 'finds selector in page' do
+        visit root_path
+        find("#dashboard").click
+        find("#support").click
+        expect(page).to have_selector :css, '#support'
+      end
+      
+    end
+    
+    describe "Support" do
+      
+      sign_as
+    
+      it 'finds selector in page' do
+        visit root_path
+        find("#dashboard").click
+        find("#support").click
+        expect(page).to have_selector :css, '#support'
+      end
+      
+    end
+    
+  end 
 
 end
